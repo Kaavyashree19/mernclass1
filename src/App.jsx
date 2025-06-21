2e312280ef10a9bfab01be538339e1f7d285f5e1
kaavya <kaavyashreer.23eie@kongu.edu>
2025-06-21 19:14:12 +0530

    fourth

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Pages from "./components/Pages.jsx"
+import Login from "./components/Login.jsx";
+import AfterLogin from "./components/AfterLogin.jsx";
+function App(){
+  let login =false;
+  return login ? <Login/> : <AfterLogin/>
+  }
+export default App;
